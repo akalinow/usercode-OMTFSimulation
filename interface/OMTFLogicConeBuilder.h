c114810ca2b46c2d5138e0d4b718753b09b68037
akalinow <Artur.Kalinowski@cern.ch>
2014-09-19 09:28:53 +0200

    a

---
+++ interface/OMTFLogicConeBuilder.h
@@ -0,0 +1,27 @@
+#ifndef OMTF_OMTFLogicConeBuilder_H
+#define OMTF_OMTFLogicConeBuilder_H
+
+#include "interface/OMTFinputMaker.h"
+
+class MtfCoordinateConverter;
+class OMTFinput;
+
+class OMTFLogicConeBuilder{
+
+ public: 
+
+  OMTFLogicConeBuilder();
+
+  ~OMTFLogicConeBuilder();
+
+  void initialize(const edm::EventSetup& es);
+
+  void buildInputForProcessor(const OMTFinputMaker::VDigiSpec & vDigi,
+			      unsigned int iProcessor);
+
+ private:
+
+  MtfCoordinateConverter *myPhiConverter;
+
+};
+#endif

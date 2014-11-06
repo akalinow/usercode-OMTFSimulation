646f59a034d8d6716f77b7b13b9921975324b12b
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2014-11-06 11:08:51 +0100

    a

---
+++ plugins/OMTFProducer.cc
@@ -0,0 +1,116 @@
+#include <iostream>
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DataFormats/L1GlobalMuonTrigger/interface/L1MuRegionalCand.h"
+
+#include "UserCode/OMTFSimulation/plugins/OMTFProducer.h"
+#include "UserCode/OMTFSimulation/interface/OMTFProcessor.h"
+#include "UserCode/OMTFSimulation/interface/OMTFinputMaker.h"
+#include "UserCode/OMTFSimulation/interface/OMTFinput.h"
+#include "UserCode/OMTFSimulation/interface/OMTFSorter.h"
+#include "UserCode/OMTFSimulation/interface/OMTFConfiguration.h"
+#include "UserCode/OMTFSimulation/interface/OMTFConfigMaker.h"
+#include "UserCode/OMTFSimulation/interface/XMLConfigWriter.h"
+
+using namespace L1TMuon;
+
+OMTFProducer::OMTFProducer(const edm::ParameterSet& cfg):
+  theConfig(cfg),
+  trigPrimSrc(cfg.getParameter<edm::InputTag>("TriggerPrimitiveSrc")){
+
+  produces<std::vector<L1MuRegionalCand> >("OMTF");
+
+  inputToken = consumes<TriggerPrimitiveCollection>(trigPrimSrc);
+  
+  if(!theConfig.exists("omtf")){
+    edm::LogError("OMTFProducer")<<"omtf configuration not found in cfg.py";
+  }
+  
+  myInputMaker = new OMTFinputMaker();
+  mySorter = new OMTFSorter();
+  
+  myWriter = new XMLConfigWriter();
+  std::string fName = "OMTF_Events";
+  myWriter->initialiseXMLDocument(fName);
+
+  dumpResultToXML = theConfig.getParameter<bool>("dumpResultToXML");
+  dumpGPToXML = theConfig.getParameter<bool>("dumpGPToXML");
+  makeConnectionsMaps = theConfig.getParameter<bool>("makeConnectionsMaps");
+
+  myOMTFConfig = 0;
+}
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+OMTFProducer::~OMTFProducer(){
+
+  delete myOMTFConfig;
+  delete myOMTFConfigMaker;
+  delete myOMTF;
+
+}
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+void OMTFProducer::beginJob(){
+
+  if(theConfig.exists("omtf")){
+    myOMTFConfig = new OMTFConfiguration(theConfig.getParameter<edm::ParameterSet>("omtf"));
+    myOMTFConfigMaker = new OMTFConfigMaker(theConfig.getParameter<edm::ParameterSet>("omtf"));
+    myOMTF = new OMTFProcessor(theConfig.getParameter<edm::ParameterSet>("omtf"));
+  }
+}
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////  
+void OMTFProducer::produce(edm::Event& iEvent, const edm::EventSetup& aESetup){
+
+  edm::Handle<TriggerPrimitiveCollection> trigPrimitives;
+  iEvent.getByToken(inputToken, trigPrimitives);
+
+  std::auto_ptr<std::vector<L1MuRegionalCand> > myCands(new std::vector<L1MuRegionalCand>);
+
+  if(dumpResultToXML) aTopElement = myWriter->writeEventHeader(iEvent.id().event());
+
+  ///Loop over all processors, each covering 60 deg in phi
+  for(unsigned int iProcessor=0;iProcessor<6;++iProcessor){
+    
+    edm::LogInfo("OMTF ROOTReader")<<"iProcessor: "<<iProcessor;
+    
+    const OMTFinput *myInput = myInputMaker->buildInputForProcessor(*trigPrimitives,iProcessor);
+    
+    ///Input data with phi ranges shifted for each processor, so it fits 10 bits range
+    const OMTFinput myShiftedInput =  myOMTF->shiftInput(iProcessor,*myInput);	
+    
+    ///Phi maps should be made with original, global phi values.
+    if(makeConnectionsMaps) myOMTFConfigMaker->makeConnetionsMap(iProcessor,*myInput);
+    /////////////////////////
+    
+    ///Results for each GP in each logic region of given processor
+    const std::vector<OMTFProcessor::resultsMap> & myResults = myOMTF->processInput(iProcessor,myShiftedInput);
+    
+    L1MuRegionalCand myOTFCandidate = mySorter->sortProcessor(myResults);
+    ////Swith from internal processor 10bit scale to global one
+    int procOffset = OMTFConfiguration::globalPhiStart(iProcessor);
+    if(procOffset<0) procOffset+=OMTFConfiguration::nPhiBins;
+    myOTFCandidate.setPhiValue(myOTFCandidate.phiValue()+OMTFConfiguration::globalPhiStart(iProcessor)+511);
+    //////////////////
+    if(myOTFCandidate.ptValue()) myCands->push_back(myOTFCandidate); 
+    
+    ///Write to XML
+    if(dumpResultToXML){
+      xercesc::DOMElement * aProcElement = myWriter->writeEventData(aTopElement,iProcessor,myShiftedInput);
+      for(unsigned int iRegion=0;iRegion<6;++iRegion){
+	///Dump only regions, where a candidate was found
+	L1Obj myCand = mySorter->sortRegionResults(myResults[iRegion]);
+	if(myCand.pt){
+	  myWriter->writeCandidateData(aProcElement,iRegion,myCand);
+	  for(auto & itKey: myResults[iRegion]) myWriter->writeResultsData(aProcElement, 
+									   iRegion,
+									   itKey.first,itKey.second);    
+	}
+      }
+    }
+  }
+  iEvent.put(myCands, "OMTF");
+}
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////  
---
+++ test/runOMTFProducer.py
@@ -0,0 +1,103 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("MakePatterns")
+import os
+import sys
+
+#inputFiles =  cms.vstring("/home/akalinow/scratch/CMS/OverlapTrackFinder/Crab/SingleMuFullEtaTestSample/Tree/v1/data//l1RpcTree_p19_p_v1_FullEta_1_1_mqa.root")
+inputFiles =  cms.vstring("/home/akalinow/scratch/CMS/OverlapTrackFinder/Crab/SingleMuFullEtaTestSample/Tree/v1/data//l1RpcTree_p10_p_v1_FullEta_1_1_Ylg.root")
+
+#inputFiles =  cms.vstring("/home/akalinow/scratch/CMS/OverlapTrackFinder/Crab/SingleMuFullEtaTestSample/Tree/v1/data//l1RpcTree_p6_p_v1_FullEta_1_1_hdg.root",
+#                          #"/home/akalinow/scratch/CMS/OverlapTrackFinder/Crab/SingleMuFullEtaTestSample/Tree/v1/data//l1RpcTree_p6_m_v1_FullEta_1_1_YpO.root"
+#                         )
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger = cms.Service("MessageLogger",
+       suppressInfo       = cms.untracked.vstring('AfterSource', 'PostModule'),
+       destinations   = cms.untracked.vstring(
+                                             'detailedInfo'
+                                             ,'critical'
+                                             ,'cout'
+                    ),
+       categories = cms.untracked.vstring(
+                                        'CondDBESSource'
+                                        ,'EventSetupDependency'
+                                        ,'Geometry'
+                                        ,'MuonGeom'
+                                        ,'GetManyWithoutRegistration'
+                                        ,'GetByLabelWithoutRegistration'
+                                        ,'Alignment'
+                                        ,'SiStripBackPlaneCorrectionDepESProducer'
+                                        ,'SiStripLorentzAngleDepESProducer'
+                                        ,'SiStripQualityESProducer'
+                                        ,'TRACKER'
+                                        ,'HCAL'
+        ),
+       critical       = cms.untracked.PSet(
+                        threshold = cms.untracked.string('ERROR') 
+        ),
+       detailedInfo   = cms.untracked.PSet(
+                      threshold  = cms.untracked.string('INFO'), 
+                      CondDBESSource  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
+                      EventSetupDependency  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
+                      Geometry  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
+                      MuonGeom  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
+                      Alignment  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
+                      GetManyWithoutRegistration  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
+                      GetByLabelWithoutRegistration  = cms.untracked.PSet (limit = cms.untracked.int32(0) ) 
+
+       ),
+       cout   = cms.untracked.PSet(
+                threshold  = cms.untracked.string('INFO'), 
+                CondDBESSource  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
+                EventSetupDependency  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
+                Geometry  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
+                MuonGeom  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
+                Alignment  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
+                GetManyWithoutRegistration  = cms.untracked.PSet (limit = cms.untracked.int32(0) ), 
+                GetByLabelWithoutRegistration  = cms.untracked.PSet (limit = cms.untracked.int32(0) ) 
+       ),
+)
+#Uncomment it to switch off messages
+##process.MessageLogger = cms.Service("MessageLogger")
+
+process.source = cms.Source(
+    'PoolSource',
+    fileNames = cms.untracked.vstring('file:/home/akalinow/scratch/CMS/OverlapTrackFinder/Crab/Test/Ipt_16m_720_FullEta_Test_v2/crab_0_141028_111133/res/SingleMu_16_m_1_1_hvk.root',
+                                      'file:/home/akalinow/scratch/CMS/OverlapTrackFinder/Crab/Test/Ipt_16m_720_FullEta_Test_v2/crab_0_141028_111133/res/SingleMu_16_m_2_1_SZ6.root'
+                                      )
+    )
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10))
+
+###PostLS1 geometry used
+process.load('Configuration.Geometry.GeometryExtendedPostLS1Reco_cff')
+process.load('Configuration.Geometry.GeometryExtendedPostLS1_cff')
+############################
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+
+path = os.environ['CMSSW_BASE']+"/src/UserCode/OMTFSimulation/data/"
+
+
+process.load('L1Trigger.L1TMuon.L1TMuonTriggerPrimitiveProducer_cfi')
+
+process.omtfEmulator = cms.EDProducer("OMTFProducer",
+                                      TriggerPrimitiveSrc = cms.InputTag('L1TMuonTriggerPrimitives'),
+                                      dumpResultToXML = cms.bool(False),                                     
+                                      dumpGPToXML = cms.bool(False),                                     
+                                      makeConnectionsMaps = cms.bool(False),                                      
+                                      omtf = cms.PSet(
+        configXMLFile = cms.string(path+"hwToLogicLayer.xml"),
+        patternsXMLFiles = cms.vstring(path+"Patterns_chPlus.xml",path+"Patterns_chMinus.xml"),
+        )
+                                      )
+
+
+process.L1TMuonSeq = cms.Sequence( process.L1TMuonTriggerPrimitives +
+                                   process.omtfEmulator )
+
+process.L1TMuonPath = cms.Path(process.L1TMuonSeq)
+
+process.schedule = cms.Schedule(process.L1TMuonPath)
+

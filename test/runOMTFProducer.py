aa495e22bbeeb38016ad417834b6783ce619466b
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2014-11-10 16:25:05 +0100

    a

--- test/runOMTFProducer.py
+++ test/runOMTFProducer.py
@@ -3,13 +3,6 @@ process = cms.Process("MakePatterns")
 import os
 import sys
 
-#inputFiles =  cms.vstring("/home/akalinow/scratch/CMS/OverlapTrackFinder/Crab/SingleMuFullEtaTestSample/Tree/v1/data//l1RpcTree_p19_p_v1_FullEta_1_1_mqa.root")
-inputFiles =  cms.vstring("/home/akalinow/scratch/CMS/OverlapTrackFinder/Crab/SingleMuFullEtaTestSample/Tree/v1/data//l1RpcTree_p10_p_v1_FullEta_1_1_Ylg.root")
-
-#inputFiles =  cms.vstring("/home/akalinow/scratch/CMS/OverlapTrackFinder/Crab/SingleMuFullEtaTestSample/Tree/v1/data//l1RpcTree_p6_p_v1_FullEta_1_1_hdg.root",
-#                          #"/home/akalinow/scratch/CMS/OverlapTrackFinder/Crab/SingleMuFullEtaTestSample/Tree/v1/data//l1RpcTree_p6_m_v1_FullEta_1_1_YpO.root"
-#                         )
-
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger = cms.Service("MessageLogger",
        suppressInfo       = cms.untracked.vstring('AfterSource', 'PostModule'),
